159d4686381a42df5894eec4f8c047fc80b8adb0
littlehorseboy <littlehorseboy@gmail.com>
2021-04-04 19:05:46 +0800

    feat(): index、autoIndex、on-index-error、virtual

---
+++ 10-autoIndex.ts
@@ -0,0 +1,29 @@
+import {
+  connect, connection, Schema, model, Document, Model, Query, QueryWithHelpers
+} from 'mongoose';
+
+connection.once('open', () => console.log('\x1b[32m %s', 'opened mongoDB'));
+
+connection.on('error', console.error.bind(console, '\x1b[31m %s', 'connection error: '));
+
+connect('mongodb://localhost:27017/mongoose', { autoIndex: false, useNewUrlParser: true, useUnifiedTopology: true });
+
+// #region 建構 Schema
+interface DocumentAnimal extends Document {
+  name: string;
+  type: string;
+}
+
+const animalSchema = new Schema<DocumentAnimal>({
+  name: String,
+  type: String,
+}, { autoIndex: false });
+
+animalSchema.set('autoIndex', false);
+
+const Animal = model<DocumentAnimal>('Animal', animalSchema);
+// #endregion
+
+Animal.find({ name: 'dog' }).exec((error, animals) => {
+  console.log(animals);
+});
---
+++ 9-schema-index.ts
@@ -0,0 +1,32 @@
+import {
+  connect, connection, Schema, model, Document, Model, Query, QueryWithHelpers
+} from 'mongoose';
+
+connection.once('open', () => console.log('\x1b[32m %s', 'opened mongoDB'));
+
+connection.on('error', console.error.bind(console, '\x1b[31m %s', 'connection error: '));
+
+connect('mongodb://localhost:27017/mongoose', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// #region 建構 Schema
+interface DocumentAnimal extends Document {
+  name: string;
+  type: string;
+}
+
+const animalSchema = new Schema<DocumentAnimal>({
+  name: String,
+  type: String,
+});
+
+animalSchema.index({
+  name: 1,
+  type: -1,
+});
+
+const Animal = model<DocumentAnimal>('Animal', animalSchema);
+// #endregion
+
+Animal.find({ name: 'dog' }).exec((error, animals) => {
+  console.log(animals);
+});
